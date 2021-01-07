d32dfca6d8e5f05ceda12a20b9f50f279ee85a37
Chris Mitchell <cmitchel@redhat.com>
2021-01-07 08:15:45 -0500

    Add env var check to client viper init
    Signed-off-by: Chris Mitchell <cmitchel@redhat.com>

--- requests/client/config.go
+++ requests/client/config.go
@@ -3,6 +3,7 @@ package client
 import "github.com/spf13/viper"
 
 func init() {
+	viper.AutomaticEnv() //Ensure Env vars are included in viper.Get calls
 	viper.SetDefault("ACCESS_TOKEN_URL", "https://sso.redhat.com/auth/realms/redhat-external/protocol/openid-connect/token")
 	viper.SetDefault("TIMEOUT_SECONDS", 30)
 }
