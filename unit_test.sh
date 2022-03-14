50e89a4677708077e9e66da09ae269dce0a93aa0
Casey Williams <caswilli@redhat.com>
2022-03-14 10:20:44 -0400

    Updating "unit_test.sh" based on the newest bonfire example

--- unit_test.sh
+++ unit_test.sh
@@ -6,15 +6,19 @@ export GO111MODULE="on"
 go test -v -race -covermode=atomic ./...
 result=$?
 
-if [ $result != 0 ]; then
-    exit $result
-else
-    # If your unit tests store junit xml results, you should store them in a file matching format `artifacts/junit-*.xml`
-    # If you have no junit file, use the below code to create a 'dummy' result file so Jenkins will not fail
-    mkdir -p $WORKSPACE/artifacts
-    cat << EOF > $WORKSPACE/artifacts/junit-dummy.xml
-    <testsuite tests="1">
-        <testcase classname="dummy" name="dummytest"/>
-    </testsuite>
+
+# If your unit tests store junit xml results, you should store them in a file matching format `artifacts/junit-*.xml`
+# If you have no junit file, use the below code to create a 'dummy' result file so Jenkins will not fail
+mkdir -p $WORKSPACE/artifacts
+cat << EOF > $WORKSPACE/artifacts/junit-dummy.xml
+<testsuite tests="1">
+    <testcase classname="dummy" name="dummytest"/>
+</testsuite>
 EOF
+
+if [ $result -ne 0 ]; then
+  echo '====================================='
+  echo '====  ✖ ERROR: UNIT TEST FAILED  ===='
+  echo '====================================='
+  exit 1
 fi
