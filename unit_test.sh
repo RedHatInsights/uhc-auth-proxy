0cdde7120583f74e0ebc314372f18a844fa5b292
Casey Williams <caswilli@redhat.com>
2022-03-14 10:43:17 -0400

    Updating "unit_test.sh" based on the newest bonfire example

--- unit_test.sh
+++ unit_test.sh
@@ -6,6 +6,8 @@ export GO111MODULE="on"
 go test -v -race -covermode=atomic ./...
 result=$?
 
+# Move to the bonfire virtual env
+source .bonfire_venv/bin/activate
 
 # If your unit tests store junit xml results, you should store them in a file matching format `artifacts/junit-*.xml`
 # If you have no junit file, use the below code to create a 'dummy' result file so Jenkins will not fail
