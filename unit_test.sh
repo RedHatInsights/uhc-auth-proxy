21fd758b39261d3ec403bfdf953067a90427fb04
Daniel Agbay <Daniel Agbay>
2023-10-20 13:35:28 -0500

    remove statement not needed afaik, copy test results to build output

--- unit_test.sh
+++ unit_test.sh
@@ -3,7 +3,7 @@
 set -exv
 
 export GO111MODULE="on"
-go test -v -race -covermode=atomic ./...
+go test -v -race --coverprofile=coverage.txt --covermode=atomic ./...
 result=$?
 
 if [ $result -ne 0 ]; then
@@ -12,6 +12,8 @@ if [ $result -ne 0 ]; then
   echo '====================================='
   exit 1
 fi
+
+cp coverage.txt $ARTIFACTS_DIR
 
 # If your unit tests store junit xml results, you should store them in a file matching format `artifacts/junit-*.xml`
 # If you have no junit file, use the below code to create a 'dummy' result file so Jenkins will not fail
