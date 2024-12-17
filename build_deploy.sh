de3fcdafb3584fbb4e7f8cce1e4441906f868968
Petra Číhalová <pcihalov@redhat.com>
2024-12-17 23:36:50 +0100

    use temp directory for docker config

--- build_deploy.sh
+++ build_deploy.sh
@@ -7,8 +7,19 @@ IMAGE_TAG=$(git rev-parse --short=7 HEAD)
 
 docker build -t "${IMAGE}:${IMAGE_TAG}" .
 
+# Create tmp dir to store data in during job run (do NOT store in $WORKSPACE)
+export TMP_JOB_DIR=$(mktemp -d -p "$HOME" -t "jenkins-${JOB_NAME}-${BUILD_NUMBER}-XXXXXX")
+echo "job tmp dir location: $TMP_JOB_DIR"
+
+function job_cleanup() {
+    echo "cleaning up job tmp dir: $TMP_JOB_DIR"
+    rm -fr $TMP_JOB_DIR
+}
+
+trap job_cleanup EXIT ERR SIGINT SIGTERM
+
 if [[ -n "$QUAY_USER" && -n "$QUAY_TOKEN" ]]; then
-    DOCKER_CONF="$PWD/.docker"
+    DOCKER_CONF="$TMP_JOB_DIR/.docker"
     mkdir -p "$DOCKER_CONF"
     docker --config="$DOCKER_CONF" login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
     docker --config="$DOCKER_CONF" push "${IMAGE}:${IMAGE_TAG}"
